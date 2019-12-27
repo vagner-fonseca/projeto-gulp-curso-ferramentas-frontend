13f0a0756d5527e593d5772aa9c4853e4d4befd7
Daniel Tapias Morales <tapmorales@gmail.com>
2019-12-27 11:58:22 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## Usage
 
-- Describe
+- This repository was created to show how to use Gulp in a small project (and what is the problem with this approach)
+- See more: [https://serfrontend.com/cursos/workflow-frontend/index.html](https://serfrontend.com/cursos/workflow-frontend/index.html "Curso Ferramentas front-end para desenvolvedores web")
 
 # Author
 
-undefined
+Daniel Tapias Morales
 
 # License
 
-undefined
+MIT
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 ;(function(){
 
-    let students = null
-    
+    let students = null    
 
     const init = (_students) => {
-        students = _students
-        
+        students = _students        
         createDomIntoTable(_students)
-
         addListenerToButton()
     }
 
     const createDomIntoTable = (students) => {
         
-        const str = students.map( (student, i) => {
-            
+        const str = students.map( (student, i) => {            
             return `
             <tr>
                 <td>${student.nome}</td>
@@ -22,39 +18,31 @@
                     student.notas.map( nota => `<td> ${nota} </td>`).join('')
                 }
                 <td>${getMedia(...student.notas)} </td>
-
             </tr>
             `
         } ).join('')
-        //- ${student.media.toFixed(2)}
-
         document.querySelector('#content-students').innerHTML = str
     }
 
     function getMedia(){
-        // console.log(arguments)
         let notas = Array.from(arguments)
         let media = (notas.reduce( (current, sum) => {
             return current + sum
-        }) / notas.length) 
-        
+        }) / notas.length)         
         return media.toFixed(2)
     }
 
     const createDomIntoList = (luckyStudents) =>{
-        const str = luckyStudents.map( s => {
-            
+        const str = luckyStudents.map( s => {            
             return `
                 <li class="list-group-item" id="student-${s.i}">${s.nome} <span style="float:right">${s.media} </span></li>
             `
         }).join('')
 
-        document.querySelector('#results ul').innerHTML = str       
-        
+        document.querySelector('#results ul').innerHTML = str        
     }
 
-    const addListenerToButton = function(){
-        
+    const addListenerToButton = function(){        
         const $btn = document.querySelector('#btn-init')
         $btn.removeAttribute('disabled')
         $btn.addEventListener('click', e => {
@@ -68,14 +56,8 @@
                 luckyStudents.push(student[0])
             }
 
-            
-
-            //pegar o maior valor de media
             let medias = [] 
-            // luckyStudents.forEach( (lucky, i) => {
-            //     medias[i] = lucky.media                
-            //     lucky.i = i
-            // } )
+
             luckyStudents.map( (ls, i) => {
                 medias[i] = ls.media
                 ls.i = i
@@ -84,16 +66,11 @@
 
             createDomIntoList(luckyStudents)
 
-            console.log(medias)
-
             const maxValue = Math.max(...medias)
-            console.log(maxValue)
 
             const studentsWinner = luckyStudents.filter( (student, i) => {
                 return student.media === maxValue
             })
-
-            console.log(studentsWinner)
 
             if(studentsWinner.length === 1){
                 showsTheWinner(studentsWinner[0])
@@ -105,7 +82,6 @@
     }
 
     const showsTheWinner = (luckyStudent) => {
-
         document.querySelector(`#student-${luckyStudent.i}`).style.backgroundColor = 'green'
     }
 
@@ -113,38 +89,23 @@
         let r = Math.random() * (max - min + 1) + min;
         return parseInt(r)
     }
-
-    /* 
-    5 - 8
-    Math.random() * (8 - 5 + 1) + 5
-    Math.random() * 4 + 5
-    0.0 * 4 + 5 = 0 + 5 = 5
-    0.99 * 4 + 5 = 3.96 + 5 = 8.96
-    */
-
-    //carrega os dados do jon    
+     
     const getStudents = async () => {
         const url_json = 'https://serfrontend.com/fakeapi/alunos.json'
         const response = await fetch(url_json)
         let data = await response.json()
 
-        //primeiro eu tinha chamado o getMedia só na hora de mostrar na tabela
-        //mas melhor tratar aqui com um .map()
         data.map( (s) => {            
             s.media = parseFloat(getMedia(...s.notas) )
-        })
-        
+        })        
         
         if (response.status !== 200) {
             throw Error(data.detail);
         }
-
         return data
         
     } 
     getStudents()
         .then(students => init(students))
         .catch(err => console.error(err))
-
-    //console.log('oi') //acontece antes se nao tiver async/await
 })()
